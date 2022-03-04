d4e2b2ca2ae5537d7f088279885d82b893a2883a
Piyush Deshmukh <99667276+Piyush-Deshmukh@users.noreply.github.com>
2022-03-04 13:59:04 +0530

    Delete Chapter 5 Directory

--- Chapter 5/Chapter 5/Count_Sublist.py
+++
@@ -1,9 +0,0 @@
-def sublist_counter(x):
-    count = 0
-    for sublist in x:
-        if type(sublist) == list:
-            count += 1
-    return count
-
-a = [1, 2, 3, [4, 5, 6], [7, 8, 9]]
-print(sublist_counter(a))
--- Chapter 5/Chapter 5/Split_and_Join_method.py
+++
@@ -1,8 +0,0 @@
-# split method --> Converts String to List
-a = "Piyush Deshmukh 18".split() 
-print(a)
-
-
-# join method --> Converts List to String
-a = ["Piyush", "Deshmukh", "18"] 
-print(" ".join(a))
