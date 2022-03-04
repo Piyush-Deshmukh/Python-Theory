816e73dc4413321e91cb02bc42b985b930a7ca74
Piyush Deshmukh <99667276+Piyush-Deshmukh@users.noreply.github.com>
2022-03-04 14:00:10 +0530

    Add Chapter 5

---
+++ Chapter 5/is_and_equals.py
@@ -0,0 +1,11 @@
+fruits1 = ["Grapes", "Apple", "Orange"]
+fruits2 = ["Bananas", "Mango"]
+fruits3 = ["Grapes", "Apple", "Orange"]
+
+# equals --> Determines if Lists are Equal or Not
+print(fruits1 == fruits2)
+print(fruits1 == fruits3)
+
+# is --> Determines if lists are at same Memory Place or Not
+print(fruits1 is fruits2)
+print(fruits1 is fruits3)
