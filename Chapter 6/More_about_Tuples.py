ac0b7e2f90d36fb758d699e2cba2095974ec0a0e
Piyush Deshmukh <99667276+Piyush-Deshmukh@users.noreply.github.com>
2022-03-06 14:10:16 +0530

    Add Chapter 5

---
+++ Chapter 6/More_about_Tuples.py
@@ -0,0 +1,13 @@
+# num = tuple(range(1,11))
+# print(num)
+
+
+# Convert Tuple to list
+# num = list((1, 2, 3, 4, 5, 6))
+# print(num)
+
+
+# Convert Tuple to String
+# num = str((1, 2, 3, 4, 5, 6))
+# print(num)
+# print(type(num))
---
+++ Chapter 6/Tuples.py
@@ -0,0 +1,54 @@
+# Tuples are immutable
+# They are faster than Lists
+
+a = ("One", "Two", "Three")
+print(a)
+
+b = (1, 2, 3, 4, 5) 
+
+# Methods used in Tuples
+# count, index
+# len function
+# slicing 
+
+
+# for i in a:
+#     print(i)
+
+
+# Tuple with 1 element
+# num = (1) --> Integer
+# num1 = (1,) --> Tuple
+# word = ("word1") --> String
+# word2 = ("word1",) --> Tuple
+# print(type(num))
+# print(type(num1))
+# print(type(word))
+# print(type(word2))
+
+
+# Tuple without parenthesis
+# words = "word1", "word2", "word3"
+# print(type(words))
+
+
+# Tuple Unpacking
+# words = ("word1", "word2", "word3")
+# x, y, z = words
+# print(x)
+# print(y)
+# print(z)
+# print(words)
+
+
+# List inside Tuple
+# mixed = (1, 2, 3, ["word1", "word2", "word3"])
+# mixed[3].pop()
+# mixed[3].append(4)
+# print(mixed)
+
+
+# Functions in Tuples
+# print(max(b))
+# print(min(b))
+# print(sum(b))
